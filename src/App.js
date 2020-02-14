44d915457c5b73208e278257268f71e0fe8d95b0
johanlia000 <lxjohansen@gmail.com>
2020-02-14 10:49:16 -0800

    finally got the icons and buttons to work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from "reactstrap";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
+      <Footer />
     </div>
   );
 }
+
+
+function Header() {
+  return <div className='header'>
+    <div className = "left-header">
+      <div className='logo'>
+        <img className = 'logoPic' src="/Photos/logo.png" alt="PairUp Logo"/>
+      </div>
+      <div className='title'>PairUp</div>
+    </div>
+    <div className="right-header">
+      <Button className="btn-icon btn-2" color="primary" type="button">
+          <span className="btn-inner--icon">
+            <i className="ni ni-fat-add" />
+          </span>
+        </Button>
+    </div>
+  </div>
+}
+
+function Footer() {
+  return <div className='footer'>
+  </div>
+}
+
+
+
 
 
 
