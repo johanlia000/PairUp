fbdea04aec9632fd7e46ee90b7a1b0ff6660d710
johanlia000 <lxjohansen@gmail.com>
2020-02-18 14:03:40 -0800

    outline UI for app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Button } from "reactstrap";
-
+// reactstrap components
+import {
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup
+} from "reactstrap";
 
 
 function App() {
@@ -9,6 +16,9 @@ function App() {
     <div className="App">
       <Header />
       <Footer />
+      <SearchBar />
+      <TripCards />
+      
     </div>
   );
 }
@@ -22,12 +32,20 @@ function Header() {
       </div>
       <div className='title'>PairUp</div>
     </div>
+
     <div className="right-header">
-      <Button className="btn-icon btn-2" color="primary" type="button">
+      <Button 
+        className="btn-icon btn-2" 
+        color='info' 
+        type="button"
+        onClick={()=> {
+          console.log('clicked + button')
+        }}
+      >
           <span className="btn-inner--icon">
             <i className="ni ni-fat-add" />
           </span>
-        </Button>
+      </Button>
     </div>
   </div>
 }
@@ -38,7 +56,86 @@ function Footer() {
 }
 
 
+function SearchBar(props){
+  var [text, setText] = useState('')
+  return <div className='search-bar'>
+    <FormGroup>
+      <InputGroup className="mb-4">
+        <Input 
+          placeholder="Search" 
+          type="text" 
+          value={text} 
+          onChange={e=> setText(e.target.value)}
+          onKeyPress={e=> {
+            if(e.key ==='Enter') {
+              console.log(text)
+              setText('')
+            }
+          }}
+        />
 
+        <InputGroupAddon addonType="append">
+          <InputGroupText style={{backgroundColor: "#f5365c"}}
+            onClick={()=> {
+              if(text) 
+                console.log(text)
+                setText('')
+              }}
+          >
+            <i className="ni ni-zoom-split-in" style={{color: "white"}}/>
+          </InputGroupText>
+        </InputGroupAddon>
+      </InputGroup>
+    </FormGroup>
+  </div>
+}
+
+
+function TripCards(){
+  return <div className='trips'>
+    <div className='trip-card'>
+      <div className='trip-text-box'>
+        <div className='trip-text'>
+          Trip Match 1
+        </div>
+      </div>
+    </div>
+
+    <div className='trip-card'>
+      <div className='trip-text-box'>
+        <div className='trip-text'>
+          Trip Match 2
+        </div>
+      </div>
+    </div>
+
+    <div className='trip-card'>
+      <div className='trip-text-box'>
+        <div className='trip-text'>
+          Trip Match 3
+        </div>
+      </div>
+    </div>
+
+
+    <div className='trip-card'>
+      <div className='trip-text-box'>
+        <div className='trip-text'>
+          Trip Match 4
+        </div>
+      </div>
+    </div>
+
+    <div className='trip-card'>
+      <div className='trip-text-box'>
+        <div className='trip-text'>
+          Trip Match 5
+        </div>
+      </div>
+    </div>
+    
+  </div>
+}
 
 
 
